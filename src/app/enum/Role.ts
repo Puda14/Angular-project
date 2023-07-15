f22a3d6c620646c0a0a29d9da4d50a0f11d4c0cb
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2023-07-16 00:24:05 +0700

    first commit

---
+++ src/app/enum/Role.ts
@@ -0,0 +1,8 @@
+export enum Role {
+    // Customer = 'ROLE_CUSTOMER',
+    // Employee = 'ROLE_EMPLOYEE',
+    // Manager = 'ROLE_MANAGER'
+    Customer = 'CUSTOMER',
+    Employee = 'REMPLOYEE',
+    Manager = 'MANAGER'
+}
